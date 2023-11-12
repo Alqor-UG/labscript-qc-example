29a45ab2cda747424cb8f38c2f4fb421d699076a
fretchen <fred.jendrzejewski@gmail.com>
2023-11-12 13:09:02 +0100

    Add the two files from the `labscript-qc` mot folder

---
+++ config.py
@@ -0,0 +1,172 @@
+"""
+In this module we define all the configuration parameters for the mot device. 
+
+No runs are started here. The entire logic is implemented in the `spooler.py` module.
+"""
+
+from typing import Tuple, Literal, List, Optional
+from pydantic import Field, BaseModel, ValidationError
+from typing_extensions import Annotated
+
+from utils.schemes import (
+    Spooler,
+    ResultDict,
+)
+
+from .spooler import gen_script_and_globals, modify_shot_output_folder, remoteClient
+
+N_MAX_SHOTS = 1000000
+N_MAX_ATOMS = 500
+MAX_EXPERIMENTS = 1000
+
+
+class LoadInstruction(BaseModel):
+    """
+    The load instruction. As each instruction it requires the
+
+    Attributes:
+        name: The string to identify the instruction
+        wires: The wire on which the instruction should be applied
+            so the indices should be between 0 and N_MAX_WIRES-1
+        params: has to be empty
+    """
+
+    name: Literal["load"]
+    wires: Annotated[
+        List[Annotated[int, Field(ge=0, le=0)]], Field(min_length=0, max_length=1)
+    ]
+    params: Annotated[
+        List[Annotated[int, Field(ge=1, le=N_MAX_ATOMS)]],
+        Field(min_length=1, max_length=1),
+    ]
+
+
+class MeasureBarrierInstruction(BaseModel):
+    """
+    The measure and barrier instruction. As each instruction it requires the
+
+    Attributes:
+        name: The string to identify the instruction
+        wires: The wire on which the instruction should be applied
+            so the indices should be between 0 and N_MAX_WIRES-1
+        params: has to be empty
+    """
+
+    name: Literal["measure", "barrier"]
+    wires: Annotated[
+        List[Annotated[int, Field(ge=0, le=0)]], Field(min_length=0, max_length=1)
+    ]
+    params: Annotated[List[float], Field(min_length=0, max_length=0)]
+
+
+class MotExperiment(BaseModel):
+    """
+    The class that defines the mot experiments
+    """
+
+    wire_order: Literal["interleaved", "sequential"] = "sequential"
+    shots: Annotated[int, Field(gt=0, le=N_MAX_SHOTS)]
+    num_wires: Literal[1]
+    instructions: List[list]
+    seed: Optional[int] = None
+
+
+class MotSpooler(Spooler):
+    """
+    The spooler class that handles all the circuit logic.
+    """
+
+    def check_experiment(self, exper_dict: dict) -> Tuple[str, bool]:
+        """
+        Check the validity of the experiment.
+        """
+        try:
+            MotExperiment(**exper_dict)
+            return "", True
+        except ValidationError as err:
+            return str(err), False
+
+    def add_job(
+        self, json_dict: dict, status_msg_dict: dict
+    ) -> tuple[ResultDict, dict]:
+        """
+        The function that translates the json with the instructions into some circuit and executes it.
+        It performs several checks for the job to see if it is properly working.
+        If things are fine the job gets added the list of things that should be executed.
+
+        json_dict: The job dictonary of all the instructions.
+        status_msg_dict: the status dictionary of the job we are treating.
+        """
+        job_id = status_msg_dict["job_id"]
+
+        result_dict: ResultDict = {
+            "display_name": self.display_name,
+            "backend_version": self.version,
+            "job_id": job_id,
+            "qobj_id": None,
+            "success": True,
+            "status": "finished",
+            "header": {},
+            "results": [],
+        }
+
+        err_msg, json_is_fine = self.check_json_dict(json_dict)
+        if json_is_fine:
+            # check_hilbert_space_dimension
+            dim_err_msg, dim_ok = self.check_dimension(json_dict)
+            if dim_ok:
+                for exp in json_dict:
+                    exp_dict = {exp: json_dict[exp]}
+                    # prepare the shots folder
+                    remoteClient.reset_shot_output_folder()
+                    modify_shot_output_folder(job_id + "/" + str(exp))
+
+                    # Here we generate the ciruit
+                    result_dict["results"].append(self.gen_circuit(exp_dict, job_id))
+
+                status_msg_dict[
+                    "detail"
+                ] += "; Passed json sanity check; Compilation done. Shots sent to solver."
+                status_msg_dict["status"] = "DONE"
+                return result_dict, status_msg_dict
+
+            status_msg_dict["detail"] += (
+                "; Failed dimensionality test. Too many atoms. File will be deleted. Error message : "
+                + dim_err_msg
+            )
+            status_msg_dict["error_message"] += (
+                "; Failed dimensionality test. Too many atoms. File will be deleted. Error message :  "
+                + dim_err_msg
+            )
+            status_msg_dict["status"] = "ERROR"
+            return result_dict, status_msg_dict
+        else:
+            status_msg_dict["detail"] += (
+                "; Failed json sanity check. File will be deleted. Error message : "
+                + err_msg
+            )
+            status_msg_dict["error_message"] += (
+                "; Failed json sanity check. File will be deleted. Error message : "
+                + err_msg
+            )
+            status_msg_dict["status"] = "ERROR"
+        return result_dict, status_msg_dict
+
+
+# This is the spooler object that is used by the main function.
+spooler_object = MotSpooler(
+    ins_schema_dict={
+        "barrier": MeasureBarrierInstruction,
+        "measure": MeasureBarrierInstruction,
+        "load": LoadInstruction,
+    },
+    n_wires=1,
+    version="0.1",
+    description="Setup of an atomic mot.",
+    n_max_experiments=MAX_EXPERIMENTS,
+    n_max_shots=N_MAX_SHOTS,
+    operational=True,
+)
+
+# Now also add the function that generates the circuit
+spooler_object.gen_circuit = gen_script_and_globals
---
+++ spooler.py
@@ -0,0 +1,169 @@
+"""
+This is the file that creates the labscript file and sends it to the BLACS.
+"""
+import os
+from time import sleep
+from decouple import config
+import numpy as np
+
+import runmanager.remote
+from lyse import Run
+
+from utils.schemes import (
+    ExperimentDict,
+    create_memory_data,
+)
+
+
+remoteClient = runmanager.remote.Client()
+
+# remote files
+REMOTE_BASE_PATH = "."
+RECEIVED_JSON_FOLDER = f"{REMOTE_BASE_PATH}"
+EXECUTED_JSON_FOLDER = f"{REMOTE_BASE_PATH}/executed"
+JSON_STATUS_FOLDER = f"{REMOTE_BASE_PATH}/status"
+
+
+# in the labscript ini file this is equivalent to the the path `labscriptlib`
+
+EXP_SCRIPT_FOLDER = config("EXP_SCRIPT_FOLDER")
+
+# local files
+HEADER_PATH = f"{EXP_SCRIPT_FOLDER}/header.py"
+
+# how long should we wait for runs until we have a look again ?
+T_WAIT = 2
+
+
+def get_file_queue(dir_path: str) -> list:
+    """
+    A function that returns the list of files in the directory.
+    """
+    files = list(fn for fn in next(os.walk(dir_path))[2])
+    return files
+
+
+def modify_shot_output_folder(new_dir: str) -> None:
+    """
+    I am not sure what this function does.
+
+    Args:
+        new_dir: The new directory under which we save the shots.
+    """
+    defaut_shot_folder = str(remoteClient.get_shot_output_folder())
+    print(f"Default shot folder: {defaut_shot_folder}")
+    modified_shot_folder = (defaut_shot_folder.rsplit("\\", 1)[0]) + "/" + new_dir
+    print(f"Modified shot folder: {modified_shot_folder}")
+    remoteClient.set_shot_output_folder(modified_shot_folder)
+
+
+def gen_script_and_globals(json_dict: dict, job_id: str) -> ExperimentDict:
+    """
+    This is the main script that generates the labscript file.
+
+    Args:
+        json_dict: The dictionary that contains the instructions for the circuit.
+        job_id: The user id of the user that is running the experiment.
+
+    Returns:
+        The path to the labscript file.
+    """
+    exp_name = next(iter(json_dict))
+    ins_list = json_dict[next(iter(json_dict))]["instructions"]
+    n_shots = json_dict[next(iter(json_dict))]["shots"]
+
+    globals_dict = {
+        "job_id": "guest",
+        "shots": 4,
+    }
+    globals_dict["shots"] = np.arange(n_shots)
+    globals_dict["job_id"] = job_id
+
+    remoteClient.set_globals(globals_dict)
+    script_name = f"experiment_{globals_dict['job_id']}.py"
+    exp_script = os.path.join(EXP_SCRIPT_FOLDER, script_name)
+    ins_list = json_dict[next(iter(json_dict))]["instructions"]
+    print(f"File path: {exp_script}")
+    code = ""
+    # this is the top part of the script it allows us to import the
+    # typical functions that we require for each single sequence
+    with open(HEADER_PATH, "r", encoding="UTF-8") as header_file:
+        code = header_file.read()
+
+    # add a line break to the code
+    code += "\n"
+    # pylint: disable=bare-except
+    try:
+        with open(exp_script, "w", encoding="UTF-8") as script_file:
+            script_file.write(code)
+    except:
+        print("Something wrong. Does file path exists?")
+
+    for inst in ins_list:
+        # we can directly use the function name as we have already verified
+        # that the function exists in the `add_job` function
+        func_name = inst[0]
+        params = "(" + str(inst[1:])[1:-1] + ")"
+        code = "Experiment." + func_name + params + "\n"
+
+        # we should add proper error handling here
+        # pylint: disable=bare-except
+        try:
+            with open(exp_script, "a", encoding="UTF-8") as script_file:
+                script_file.write(code)
+        except:
+            print("Something wrong. Does file path exists?")
+
+    code = "Experiment.final_action()" + "\n" + "stop(Experiment.t+0.1)"
+    # pylint: disable=bare-except
+    try:
+        with open(exp_script, "a", encoding="UTF-8") as script_file:
+            script_file.write(code)
+    except:
+        print("Something wrong. Does file path exists?")
+    remoteClient.set_labscript_file(
+        exp_script
+    )  # CAUTION !! This command only selects the file. It does not generate it!
+
+    # be careful. This is not a blocking command
+    remoteClient.engage()
+
+    # now that we have engaged the calculation we need to wait for the
+    # calculation to be done
+
+    # we need to get the current shot output folder
+    current_shot_folder = remoteClient.get_shot_output_folder()
+
+    # we need to get the list of files in the folder
+    hdf5_files = get_file_queue(current_shot_folder)
+
+    # we need to wait until we have the right number of files
+    while len(hdf5_files) < n_shots:
+        sleep(T_WAIT)
+        hdf5_files = get_file_queue(current_shot_folder)
+
+    shots_array = []
+    # once the files are there we can read them
+    for file in hdf5_files:
+        run = Run(current_shot_folder + "/" + file)
+        got_nat = False
+        n_tries = 0
+        # sometimes the file is not ready yet. We need to wait a bit
+        while not got_nat and n_tries < 15:
+            # the exception is raised if the file is not ready yet
+            # it is broadly defined within labscript so we cannot do anything about
+            # it here.
+            # pylint: disable=W0718
+            try:
+                print(run.get_results("/measure", "nat"))
+                # append the result to the array
+                shots_array.append(run.get_results("/measure", "nat"))
+                got_nat = True
+            except Exception as exc:
+                print(exc)
+                sleep(T_WAIT)
+                n_tries += 1
+    print(f"Shots array: {shots_array}")
+
+    exp_sub_dict = create_memory_data(shots_array, exp_name, n_shots)
+    return exp_sub_dict
